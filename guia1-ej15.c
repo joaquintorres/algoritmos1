87f3fae013324a8c07871e4ab2af77109982d48c
joaquintorres <joaquintorres1997@gmail.com>
2018-04-07 00:50:49 -0300

    Problema 15 ahora funcional.

--- guia1-ej15.c
+++ guia1-ej15.c
@@ -1,22 +1,36 @@
 #include<stdio.h>
+#define ENTRADA 5
+ 
 int main(void)
 {
+	int i;
 	int j;
-	int entrada;
-	puts("Ingrese el número (entero) de entradas a promediar:");
-	j = scanf("%i",&entrada);
-	/*ampersand señala la direccion de memoria de la variable
-	 "entrada",j indica si salió bien */
-	if (!j)
+	
+	float numero_parcial;
+	float total = 0;
+	float promedio;
+	/*¿cómo haría para que sirva para números muy grandes?*/
+	printf("Ingrese %i números para promediar.\n", ENTRADA);
+	for (i = 0; i < ENTRADA; i++)
 	{
-		fprintf(stderr, "%s\n", "ERROR: Entrada inválida.");
-		return 0;
-	}
-	if (entrada < )
-	{
-		/* code */
-	}
-	printf("Contador: %i\n", entrada);
+		j = scanf("%f", &numero_parcial);
+		/*ampersand señala la direccion de memoria de la variable
+	 	"numero_parcial",j indica si salió bien, pero numero_parcial es donde se 
+	 	almacena el input */
+		if (!j)
+		{
+			fprintf(stderr, "%s\n", "ERROR: Entrada inválida. Los números con decimal se escriben con punto en lugar de coma.");
+			return 1;
+		}
 
+		if (numero_parcial < 0)
+		{
+			fprintf(stderr, "%s\n", "ERROR: número negativo.");
+			return 1;
+		}
+		total += numero_parcial;
+	}
+	promedio = total / ENTRADA;
+	printf("El promedio es: %f\n", promedio);
 	return 0;
 }
