b77d6c594bd9ac9425f16fffddb80520ce5d81db
joaquintorres <joaquintorres1997@gmail.com>
2018-04-10 20:32:13 -0300

    Termina ej 11

--- guia1-ej11.c
+++ guia1-ej11.c
@@ -3,11 +3,23 @@
 int main(void)
 {
 	int n;
+	/* número a ser invertido*/
+	int x = 0;
+	/* variable que almacena el resto*/
 	scanf("%d", &n);
 	if (n<1)
 	{
 		fprintf(stderr, "%s\n", ERROR_MSG);
 		return 1;
 	}
+	while (n != 0)
+	{
+		x = x*10 + n%10;
+		/*toma el resto n%10 y lo acumula multiplicando por 10
+		para poder sumarlo apropiadamente*/
+		n = n/10;
+		/*el multiplo restante*/
+	}
+	printf("%d\n", x);	
 	return 0;
 }
