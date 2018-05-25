1abbb88a817467631c6cf91fac2321ea6b68cf8f
joaquintorres <joaquintorres1997@gmail.com>
2018-05-25 14:46:19 -0300

    abm ahora tiene core dumps

--- abm/altas_bajas.c
+++ abm/altas_bajas.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MIN_ARGUMENTS 5
 #define MAX_ARGUMENTS 5
@@ -11,7 +13,9 @@
 #define CMD_ARG_APPENDED_FILE_TOKEN_VALUE 4
 #define CMD_ARG_APPENDED_FILE_FLAG "-a"
 
-#define TEMP_FILENAME "temp.tmp"
+#define TEMP_FILENAME "salida.tmp"
+#define INIT_CHOP 20
+#define CHOP_SIZE 5
 typedef enum {
 	OK,
 	ERROR_NULL_POINTER,
@@ -23,6 +27,11 @@ typedef enum {
 	ERROR_INPUT_FILE,
 	ERROR_DISK_SPACE
 } status_t;
+
+typedef enum{
+	TRUE,
+	FALSE
+} bool_t;
 
 status_t validate_arguments(int argc, char * argv[], char ** inventory_file_path, char ** appended_file_path)
 {
@@ -43,15 +52,47 @@ status_t validate_arguments(int argc, char * argv[], char ** inventory_file_path
 	return OK;
 }
 
+status_t read_line(char ** s, bool_t * eof)
+{
+	size_t used_size;
+	size_t alloc_size;
+	char c;
+	char * aux;
+	if (s == NULL || eof == NULL)
+		return ERROR_NULL_POINTER;
+	if ((*s = (char *) malloc(INIT_CHOP*sizeof(char))) == NULL)
+		return ERROR_MEMORY;
+	alloc_size = INIT_CHOP;
+	used_size = 0;
+	while ((c = getchar())!= '\n' && c != EOF)
+	{
+		if (used_size == alloc_size - 1)
+		{
+			if ((aux = (char *) realloc(s, (alloc_size + CHOP_SIZE)*sizeof(char))) == NULL)
+			{
+				free(*s);
+				*s = NULL;
+				return ERROR_MEMORY;
+			}
+			*s = aux;
+			alloc_size += CHOP_SIZE;
+		}
+		(*s)[used_size++] = c;
+	}
+	*eof = (c == EOF) ? TRUE:FALSE;
+	return OK;	
+}
+
 int main(int argc, char *argv[])
 {
 	char * inventory_file_path;
 	char * appended_file_path;
+	char * line;
 	FILE * inventory_file_ptr;
 	FILE * appended_file_ptr;
 	FILE * crud_file_ptr;  /*Puntero al archivo resultante de aplicar ABM (o CRUD por sus siglas en inglés)*/
 	status_t st;
-
+	bool_t eof; /*booleano que indica si en la lectura se llegó a EOF*/
 	if ((st = validate_arguments(argc, argv, &inventory_file_path, &appended_file_path)) != OK)
 		return st;
 	/*APERTURA*/
@@ -68,11 +109,22 @@ int main(int argc, char *argv[])
 		fclose(appended_file_ptr);
 		return ERROR_INPUT_FILE;
 	}
+	/*LECTURA*/
+	eof = FALSE;
+	while (eof == FALSE)
+	{
+		/*if((st = read_line(&line,&eof)) !=OK)
+			return st;*/
+		read_line
+		puts(line);
+		free(line);
+	}
+	/*ABM*/
 
 	/*CIERRE*/
 	fclose(inventory_file_ptr);
 	fclose(appended_file_ptr);
-	if ((fclose(crud_file_ptr)) == NULL)
+	if ((fclose(crud_file_ptr)) == EOF)
 		return ERROR_DISK_SPACE;
 
 
