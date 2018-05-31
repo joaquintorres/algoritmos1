f530b124892f1a91823c63dcd508ee1db4f37813
Joaquin Torres <jtorres@lpc2.labs.df.uba.ar>
2018-05-31 12:11:30 -0300

    Primer ejercicio de memoria dinámica que funciona.

--- guia8-ej1.c
+++ guia8-ej1.c
@@ -16,7 +16,7 @@ typedef enum {
 	ERROR_INVALID_FLAG
 } status_t;
 
-status_t validate_arguments (int argc, char *argv[], char ** string)
+status_t validate_arguments(int argc, char *argv[], char ** string)
 {
 	if (argv == NULL || string == NULL)
 		return ERROR_NULL_POINTER;
@@ -28,13 +28,32 @@ status_t validate_arguments (int argc, char *argv[], char ** string)
 	return OK;
 }
 
+status_t clone_string(const char * string, char ** copy)
+{
+	if (string == NULL || copy == NULL)
+		return ERROR_NULL_POINTER;
+	if((*copy = (char *) malloc((strlen(string) + 2) * sizeof (char))) == NULL)
+	{
+			free(*copy);
+			*copy = NULL;
+			return ERROR_MEMORY;
+	}
+	if (strcpy(*copy, string) == NULL)
+		return ERROR_NULL_POINTER;
+	return OK;
+}
+
 int main(int argc, char * argv[])
 {
 	char * string;
+	char * copy;
 	status_t st;
 	if ((st = validate_arguments(argc, argv, &string)) != OK)
-		return(st);
+		return st;
 	printf("%s\n", string);
+	if ((st = clone_string(string,&copy)) != OK)
+		return st;
+	printf("%s\n", copy);
 	return OK;
 }
 
