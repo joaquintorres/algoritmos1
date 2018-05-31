eb96575857dedae89d1f88a52d7fbf53ddff0ef8
Joaquin Torres <jtorres@lpc2.labs.df.uba.ar>
2018-05-31 12:52:36 -0300

    test de cómo usar tm, localtime y time.

--- sysdate/test.c
+++ sysdate/test.c
@@ -3,6 +3,12 @@
 
 int main(void)
 {
-	puts(tm.tm_sec);
+	time_t raw_time;
+	struct tm * cal_ptr;
+	if ((raw_time = time(NULL)) == -1)
+		return 1;
+	if((cal_ptr = localtime(&raw_time)) == NULL)
+		return 1;
+	printf("Día del año: %02d Día del mes:%02d %02d:%02d:%02d\n",cal_ptr -> tm_yday, cal_ptr->tm_mday, cal_ptr->tm_hour, cal_ptr->tm_min, cal_ptr->tm_sec);
 	return 0;
 }
